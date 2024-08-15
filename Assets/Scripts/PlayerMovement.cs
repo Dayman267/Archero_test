90e7bdfd4f79e72136fadb77b6013d24cec77012
Dayman267 <108380150+Dayman267@users.noreply.github.com>
2024-08-15 11:38:24 +0300

    player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,34 +1,60 @@
-using System;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
     private Rigidbody2D rb;
     [SerializeField] Joystick joystick;
-    //private PlayerInput playerInput;
+    private Vector2 moveInput;
+    [SerializeField] private uint circlecastRadius = 20;
+    [SerializeField] private uint circlecastDistance = 30;
+    [SerializeField] private LayerMask castingMask;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-        //playerInput = GetComponent<PlayerInput>();
     }
 
+    private void FixedUpdate()
+    {
+        Walk();
+        Look();
+    }
+    
     public void Walk()
     {
-        Vector2 moveInput = joystick.Direction;
-        rb.velocity = moveInput * moveSpeed;
-        Debug.Log(joystick.Direction);
+        moveInput = joystick.Direction;
+        rb.MovePosition(rb.position + moveInput * moveSpeed * Time.fixedDeltaTime);
+    }
+    
+    private void Look()
+    {
+        if (moveInput != Vector2.zero)
+        {
+            Rotate(moveInput);
+        }
+        else
+        {
+            RaycastHit2D[] hits = Physics2D.CircleCastAll(
+                transform.position, 
+                circlecastRadius, 
+                Vector2.up, 
+                circlecastDistance, 
+                castingMask);
+            Vector2 closestEnemy = Vector2.positiveInfinity;
+            foreach (var hit in hits)
+            {
+                closestEnemy = Vector2.Distance(hit.transform.position, transform.position) < 
+                               Vector2.Distance(closestEnemy, transform.position) ? hit.transform.position : closestEnemy;
+            }
+            Vector2 direction = closestEnemy - (Vector2)transform.position;
+            Rotate(direction);
+        }
     }
 
-    private void OnEnable()
+    private void Rotate(Vector2 lookDirection)
     {
-        //playerInput.onActionTriggered += PlayerInput_onActionTriggered;
-    }
-
-    private void PlayerInput_onActionTriggered(InputAction.CallbackContext context)
-    {
-        throw new NotImplementedException();
+        float rotate = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotate - 90);
     }
 }
